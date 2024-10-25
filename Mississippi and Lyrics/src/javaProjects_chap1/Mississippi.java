12916f1053afedb0af1e7eace4f455b751c3eb19
GM|Matrix <105758951+GMMATRIX@users.noreply.github.com>
2024-10-25 13:31:08 -0700

    mississippi and lyrics files

---
+++ Mississippi and Lyrics/src/javaProjects_chap1/Mississippi.java
@@ -0,0 +1,33 @@
+package javaProjects_chap1;
+
+public class Mississippi {
+	public static void main(String[] args) {
+        m();
+        i();
+        s();
+        s();
+        i();
+        s();
+        s();
+        i();
+        p();
+        p();
+        i();
+    }
+ 
+    public static void m(){
+        System.out.println("M     M\nMM   MM\nM M M M\nM  M  M\nM     M\nM     M\nM     M\n");
+    }
+ 
+    public static void i(){
+        System.out.println(" IIIII \n   I   \n   I   \n   I   \n   I   \n   I   \n IIIII \n");
+    }
+ 
+    public static void s(){
+        System.out.println(" SSSSS \nS     S\nS      \n SSSSS \n      S\nS     S\n SSSSS \n");
+    }
+    public static void p(){
+        System.out.println("PPPPPP\nP     P\nP     P\nPPPPPP\nP      \nP      \nP      \n");
+    }
+
+}
